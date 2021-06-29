28a340475b84651c7e7d15163530455e547b0f29
MarceloGuzman <felix0085@live.cl>
2021-06-29 11:04:58 -0400

    Sumativa Unidad 2

--- sumativa2/src/app/app.component.spec.ts
+++ sumativa2/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'formativa21'`, () => {
+  it(`should have as title 'sumativa2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('formativa21');
+    expect(app.title).toEqual('sumativa2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('formativa21 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('sumativa2 app is running!');
   });
 });
--- sumativa2/src/app/app.module.ts
+++ sumativa2/src/app/app.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PruebaComponent } from './prueba/prueba.component';
-import { Form2Component } from './form2/form2.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PruebaComponent,
-    Form2Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
